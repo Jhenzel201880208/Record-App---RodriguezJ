d2d6bdfe1548a60f240cfcb9d82c046353a3b27f
Jhenzel201880208 <92150970+Jhenzel201880208@users.noreply.github.com>
2022-04-11 20:04:33 +0800

    Finish

---
+++ RecordApp/config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('ROOT_URL', 'https://localhost/RecordApp');
+define('DB_HOST', 'locahost');
+define('DB_USER', 'root');
+define('DB_PASS', 'helloadmin');
+define('DB_NAME', 'recordapp_db');
